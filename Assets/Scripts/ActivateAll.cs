daf28faa71e855cc722ca6e19b8606e98779164b
pav117 <PV222@live.mdx.ac.uk>
2024-04-24 18:14:17 +0100

    Main Level Changes
    Enemies added and scripts added for rocket and terminal

--- Assets/Scripts/ActivateAll.cs
+++ Assets/Scripts/ActivateAll.cs
@@ -6,7 +6,6 @@ public class ActivateAll : MonoBehaviour
 {
     public GameObject item1;
     public GameObject item2;
-    public GameObject item3;
 
     public GameObject somethingHappens;
 
@@ -19,7 +18,7 @@ public class ActivateAll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (item1.activeInHierarchy && item2.activeInHierarchy && item3.activeInHierarchy)
+        if (item1.activeInHierarchy && item2.activeInHierarchy)
         {
             somethingHappens.SetActive(false);
         }
